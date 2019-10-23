10ee9e8b141e9537809c9a2bb31fc68414b19d17
MattTrinh <mbtrinh@asu.edu>
2019-10-22 20:56:59 -0700

    Added RPN loss functions

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ The goal of this project is to train a Faster R-CNN model with the SVHN dataset 
 
 ## Notes
 - Data files removed due to size. The assumption is that the dataset is located in the "data" folder in the main directory.
+- Region Proposal Network (RPN) implemented mostly along the lines of the original paper found [here](https://papers.nips.cc/paper/5638-faster-r-cnn-towards-real-time-object-detection-with-region-proposal-networks.pdf)
 
 ## References
 
--- src/rpn.py
+++ src/rpn.py
@@ -1,6 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from keras.layers import Conv2D
+from keras import backend as K
 
 # Creates and returns the layers for the RPN
 def create_rpn(num_anchors, shared_layers):
@@ -22,3 +23,35 @@ def create_rpn(num_anchors, shared_layers):
 def nms(boxes, scores, max_proposals, overlap_thresh):
     indices = tf.image.non_max_suppression(boxes, scores, max_proposals, overlap_thresh)
     return indices
+    
+# Loss function for bounding boxes
+def rpn_loss_box(num_anchors):
+    # Need to pass symbolic function that takes the true labels and predictions as arguments
+    # See: https://keras.io/losses/
+    def rpn_loss_box_fixed_num(y_true, y_pred):
+        balanced_weight = 1.0
+        ground_truth = y_true[:, :, :, :4 * num_anchors]
+        n_reg = K.sum(1e-4 + ground_truth)
+        
+        # Calculate Huber Loss
+        # Note that Huber Loss is a robust loss function
+        error = y_pred - y_true[:, :, :, 4 * num_anchors:]
+        abs_error = K.abs(error)
+        quadratic = K.minimum(abs_error, 1.0)
+        linear = abs_error - quadratic
+        huber_loss = 0.5 * K.square(quadratic) + 1.0 * linear
+        
+        return balanced_weight * K.sum(ground_truth * huber_loss) / n_reg
+    return rpn_loss_box_fixed
+    
+# Loss function for positive/negative regions (i.e. background or object)
+def rpn_loss_bin(num_anchors):
+    # Need to pass symbolic function that takes the true labels and predictions as arguments
+    # See: https://keras.io/losses/
+    def rpn_loss_bin_fixed_num(y_true, y_pred):
+        ground_truth = y_true[:, :, :, :num_anchors]
+        predictions = y_pred[:, :, :, :]
+        n_cls = K.sum(1e-4 + ground_truth)
+        
+        # Note that binary cross entropy loss is a log loss function
+        return K.sum(K.binary_crossentropy(predictions, ground_truth)) / n_cls
